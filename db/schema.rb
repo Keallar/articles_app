456279aca1fdc72fffe0dc95c232eda93cad04bc
keallar <zlysanskiy@gmail.com>
2022-01-15 00:36:06 +0300

    Added index and show views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_105838) do
+ActiveRecord::Schema.define(version: 2022_01_13_214323) do
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "description"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
