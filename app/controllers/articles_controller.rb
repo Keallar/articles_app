273345b1da7a901d9026669abdbd4fe20e1f3668
keallar <zlysanskiy@gmail.com>
2022-01-16 22:03:21 +0300

    Added new edit views and other chages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,30 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    
+    # Rails.logger.info('New article')
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = 'Article was created succesfully.'
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def destroy; end
+
+  private
+
+  def method_name
     
   end
+  
 end
