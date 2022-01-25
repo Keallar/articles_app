80740aebab059ed768e2e95ea9dcf0b19eefe817
keallar <zlysanskiy@gmail.com>
2022-01-25 16:51:35 +0300

    Home page with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'articles#index'
+  root 'pages#home'
   get 'about', to: 'pages#about'
-  # resources :articles, only: %i[show index new create edit update destroy]
-  # same
   resources :articles
 end
