35fb25a8938964e4cfc9254f8b0673325d1701b4
Лысанский Евгений Валерьевич <elysanskiy@phoenixit.ru>
2022-01-27 17:35:50 +0300

    First it of users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_214323) do
+ActiveRecord::Schema.define(version: 2022_01_27_140914) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2022_01_13_214323) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
