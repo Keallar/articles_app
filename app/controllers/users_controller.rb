de2755b9ff5bba0361b6a1a1580ee1ad103e3f19
Лысанский Евгений Валерьевич <elysanskiy@phoenixit.ru>
2022-01-28 16:37:16 +0300

    Added user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      flash[:notice] = "User was succesfully created"
+      Rails.logger.info("User #{@user.username} created")
+      redirect_to articles_path
+    else
+      render :new
+    end      
+  end
+
+  def destroy
+    
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
   
 end
