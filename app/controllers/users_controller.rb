3326a8b449fbd905bb086e4c66e237662a66b373
keallar <elysanskiy@phoenixit.ru>
2022-02-18 00:03:19 +0300

    Added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   def destroy 
     @user.destroy
     Rails.logger.info("Article #{@user.username} deleted")
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = 'Account and all associated articles deleted'
     redirect_to root
   end
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = 'You can only edit or delete your profile'
       redirect_to current_user
     end
