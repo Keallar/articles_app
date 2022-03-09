234e77fc99e9c174621fa5c5697e3fb56a95b0a7
Keallar <zlysanskiy@gamil.com>
2022-03-10 00:34:58 +0300

    Some changes

---
+++ spec/controllers/categories_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+  describe 'GET /index' do
+    it "should get index" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /new' do
+    it "should get new" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    # let(:category) { create(:category) }
+
+    it 'should show category' do
+      @category = Category.create(name: 'Sports')
+      get :show, id: @category.id
+      expect(category).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /create' do
+    it 'should create category' do
+
+    end
+  end
+end
