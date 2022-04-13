29cfe1af4381f025efad1b0449b4f528d1c2c83b
Лысанский Евгений Валерьевич <elysanskiy@phoenixit.ru>
2022-04-13 18:21:02 +0300

    Fix spec tests, added gem faker

--- Gemfile
+++ Gemfile
@@ -60,15 +60,11 @@ group :development, :test do
   gem "debug", platforms: %i[mri mingw x64_mingw]
   # Use sqlite3 as the database for Active Record
   gem "sqlite3", "~> 1.4"
-
   gem 'byebug'
-
   gem 'rspec-rails'
-
-  gem 'factory_bot'
-
+  gem 'factory_bot_rails'
+  gem 'faker'
   gem 'rails-controller-testing'
-
   gem 'capybara'
 end
 
--- spec/integrations/create_category_spec.rb
+++ spec/integrations/create_category_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe 'Category new and create', :type => :request do
-  # let(:create_category) { Category.create(name: 'Sports') }
+  let(:create_category) { Category.create(name: 'Sports') }
 
   it 'get new category form and create category' do
     get '/categories/new'
     expect(response).to have_http_status(:success)
-    # expect { create_category }.to change { Category.count }.by(1)
-    # expect(response.body).to eq('Traveling')
-    post '/categories', params: { category: { name: 'Traveling' } }
-    expect(assigns(:categories)).to eq 1
+    expect {
+      post '/categories', params: {category: { name: 'Traveling' } }
+    }.to change{ Category.count }.by(1)
     expect(response).to redirect_to(assigns(:category))
-    # response.should redirect_to("/categories/#{Category.count}")
-    # expect(response).to eq('Traveling')
+    follow_redirect!
+    expect(response).to render_template(:show)
   end
 
+  let(:create_error_category) { Category.create(name: '') }
+
   it 'get new category form and reject invalid category submission' do
-    get 'categories/new'
-    expect(response)
+    get '/categories/new'
+    expect(response).to have_http_status(:success)
+    expect {
+      post '/categories', params: {category: { name: '' } }
+    }.to change{ Category.count }.by(0)
   end
 end
